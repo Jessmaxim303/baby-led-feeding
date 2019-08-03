58e8f54238a7ac5fd54c6137dea01277e2ba62a0
Jesse Maxim <jessmaxim303@gmail.com>
2019-08-02 20:45:57 -0600

    getAge function working

--- main.js
+++ main.js
@@ -9,8 +9,31 @@ function mainEvent() {
 	 infoCard.insertAdjacentHTML('afterbegin',
     `<container id="info__card--container">
       <div id="card__container">
-       <h3>Hello</h3>
+       <h3>
+       	${nameInput.value}
+       	${ageInput.value}
+       </h3>
+      	 <p>
+      		 If your baby is ready to start solids, you may want to try 
+      		 baby-led weaning, relying on table foods rather than purees. 
+      		 Here are five great baby-led weaning foods to test out—no cooking required!
+      	 </p>
        <div>
      </container>`);
+	 getAge();
 };
+
+function getAge() {
+	var ageData = ageInput.value;
+	var stringData = ageData.toString();
+	var ageArray = stringData.split(' ');
+	console.log(ageArray[0]);
+	if (ageArray[0] < 6) {
+		console.log('Food before one');
+	} else {
+		console.log('hello hunter');
+	}
+};
+
+
 
