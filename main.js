25cad3b95c8d2b6d550b73fc907839eded6f6f2a
Jesse Maxim <jessmaxim303@gmail.com>
2019-08-04 20:25:32 -0600

    getAge function working

--- main.js
+++ main.js
@@ -17,14 +17,15 @@ mainButton.addEventListener("click", mainEvent);
 function mainEvent() {
   newMenuContainer();
   getAge();
-  insertSixMonths();
+  // insertSixMonths();
   insertAllergyInfo();
 }
 
 function newMenuContainer() {
-  document.getElementById('main__article--container').style.width = "20%";
-  document.getElementById('main__article--container').style.marginLeft = "10%";
-  document.getElementById('main__article--container').style.background = "#212221";
+  document.getElementById('main__article--container').style.width = "80px";
+  // document.getElementById('main__article--container').style.marginLeft = "10%";
+  document.getElementById('main__article--container').style.background = "#0F709B";
+  document.getElementById('main__info--card').style.width = "60%";
   document.getElementById('main__content').innerText = " ";
   insertHomeIcon();
   // document.getElementById('main__info--card').style.width = "50%";
@@ -32,10 +33,18 @@ function newMenuContainer() {
   // document.getElementById('main__name--input').style.width = "90%";
   // document.getElementById('main__age--input').style.width = "90%";
   // document.getElementById('main__age--button').style.width = "90%";
-
   // document.querySelector('main__input--text').innerText = 'Enter Name <br> and Age';
   // document.getElementById('card__container').style.width = '20%';
 };
+
+// function allergyContainer() {
+//   document.getElementById('main__article--container').style.width = "80px";
+//   // document.getElementById('main__article--container').style.marginLeft = "10%";
+//   document.getElementById('main__article--container').style.background = "#0F709B";
+//   document.getElementById('main__info--card').style.width = "60%";
+//   document.getElementById('main__content').innerText = " ";
+//   insertHomeIcon();
+// };
 
 function insertHomeIcon() {
    imagesHomeArticle.insertAdjacentHTML('afterbegin',
@@ -46,7 +55,18 @@ function insertHomeIcon() {
      </container>`);
 };
 
-function insertSixMonths() {
+function insertAllergyInfo() {
+  allergyCard.insertAdjacentHTML('afterbegin',
+    `<container id="main__allergy--card">
+      <div id="allergy__container">
+         <div id="allergyIcon">
+        <img class="images__allergy--icon" src="images/hospital.png">
+      </div>
+       <div>
+     </container>`);
+};
+
+function insertBeforeSixMonths() {
 	 infoCard.insertAdjacentHTML('afterbegin',
     `<container id="info__card--container">
       <div id="card__container">
@@ -55,37 +75,144 @@ function insertSixMonths() {
          	${ageInput.value}
         </h2>
        	 <p>
-      		 If your baby is ready to start solids, you may want to try 
-      		 baby-led weaning, relying on table foods rather than purees. 
-      		 Here are five great baby-led weaning foods to test out—no cooking required!
+      		 Not recommended for babies under 6 months. 
+           Food before one is just for fun.
       	 </p>
        <div>
      </container>`);
 };
 
-function insertAllergyInfo() {
-  allergyCard.insertAdjacentHTML('afterbegin',
-    `<container id="main__allergy--card">
-      <div id="allergy__container">
-         <p>
-           If your baby is ready to start solids.
-         </p>
+function insertSixMonths() {
+   infoCard.insertAdjacentHTML('afterbegin',
+    `<container id="info__card--container">
+      <div id="card__container">
+        <h2>
+          ${nameInput.value}
+          ${ageInput.value}
+        </h2>
+          <h3>
+            Motor skills around 6 months old: moving food back and forth in the mouth.
+          </h3>
+         <h3> 
+          Food before 1 if just for fun.
+         </h3>
+        <p>
+          Start to introduce them to food. 
+          Make a goal of 100 foods in 100 days..
+        </p>
+        <p>
+          There are no hard-and-fast rules anymore about what to feed your baby first 
+          (except to steer clear of honey and choking hazards). Whether you choose to give your baby a vegetable puree, 
+          rice cereal, pureed chicken, or a wedge of watermelon to gnaw on, offer him just one new 
+          food every three days, and watch closely for signs of allergic reaction.
+        </p>
        <div>
      </container>`);
-}
+};
+
+function insertEightMonths() {
+   infoCard.insertAdjacentHTML('afterbegin',
+    `<container id="info__card--container">
+      <div id="card__container">
+        <h2>
+          ${nameInput.value}
+          ${ageInput.value}
+        </h2>
+          <h3>
+            Motor skills between 7-8 months: gaining skills, confidence and appetite.
+          </h3>
+          <p>
+            Start introducing them to some of the food allergy.
+          </p>
+          <p>
+            Introduce food allergens early on
+           There is no real advantage of delaying food allergen introduction.
+          </p>
+      <div>
+    </container>`);
+};
+
+function insertTenMonths() {
+   infoCard.insertAdjacentHTML('afterbegin',
+    `<container id="info__card--container">
+      <div id="card__container">
+        <h2>
+          ${nameInput.value}
+          ${ageInput.value}
+        </h2>
+        <h3>
+          Motor skills between 8-10 months: gaining skills, confidence and appetite.
+        </h3>
+        <p>
+          Start introducing them to some of the food allergy.
+        </p>
+        <p>
+          Introduce food allergens early on
+          There is no real advantage of delaying food allergen introduction.
+        </p>
+      <div>
+    </container>`);
+};
+
+function insertTwelveMonths() {
+   infoCard.insertAdjacentHTML('afterbegin',
+    `<container id="info__card--container">
+      <div id="card__container">
+        <h2>
+          ${nameInput.value}
+          ${ageInput.value}
+        </h2>
+        <h3>
+           Motor skills between 10-12 months: baby does the pincer grasp.
+        </h3>
+        <p>
+          Serve a range of food textures.
+          The amount of chewing strength and stamina required depends on food texture. 
+          Mix things up to expose baby to various textures.
+        </p>
+      <div>
+    </container>`);
+};
+
+function insertTwelvePlus() {
+   infoCard.insertAdjacentHTML('afterbegin',
+    `<container id="info__card--container">
+      <div id="card__container">
+        <h2>
+          ${nameInput.value}
+          ${ageInput.value}
+        </h2>
+        <h3>
+           Motor skills between 12+ months: baby is a pro but might learn that being messy gets your attention!.
+        </h3>
+        <p>
+          Baby eats like a pro and likely has more and more teeth, helping with the chewing.
+        </p>
+      <div>
+    </container>`);
+};
 
 function getAge() {
 	var ageData = ageInput.value;
 	var stringData = ageData.toString();
 	var ageArray = stringData.split(' ');
-	console.log(ageArray[0]);
-	if (ageArray[0] < 6) {
-		console.log('Food before one');
-	} else {
-		console.log('hello hunter');
-	}
+  if (ageArray[0] == 1) {
+    ageArray = 12;
+    insertTwelvePlus();
+  } else if (ageArray[0] < 6) {
+    insertBeforeSixMonths();
+	} else if (ageArray[0] == 6) {
+    insertSixMonths()
+  } else if (ageArray[0] <= 8) {
+    insertEightMonths();
+	} else if (ageArray[0] <= 10) {
+    insertTenMonths();
+  } else if (ageArray[0] <= 12) {
+    insertTwelveMonths();
+  } else if (ageArray[0] > 12) {
+    insertTwelvePlus();
+  }
 };
-
 
 
 
